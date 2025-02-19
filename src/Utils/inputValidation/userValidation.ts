611c53a831430f92a55e369c6a7ab250d658fd40
taras <taras@mail.com>
2025-02-20 00:01:20 +0200

    Modified userValidation file , added customized error for client and http errors

--- src/Utils/inputValidation/userValidation.ts
+++ src/Utils/inputValidation/userValidation.ts
@@ -1,62 +1,22 @@
-import CreaterUserError from "./errorHendlers.ts"
+import CreateNewError from "../../errors/errorHendlers.ts";
 
 export const UserInputValidator = (name: string, email: string, phoneNumber: string, password: string) => {
-    let error = new CreaterUserError()
+    // checking empty inputs
+    if (!name) throw new CreateNewError({ fieldName: "name", errorMessage: "Поле не повинно бути пустим" }, "Validation error");
+    if (!email) throw new CreateNewError({ fieldName: "email", errorMessage: "Поле не повинно бути пустим" }, "Validation error");
+    if (!phoneNumber) throw new CreateNewError({ fieldName: "phoneNumber", errorMessage: "Поле не повинно бути пустим" }, "Validation error");
+    if (!password) throw new CreateNewError({ fieldName: "password", errorMessage: "Поле не повинно бути пустим" }, "Validation error");
 
-    if (/[0-9]+/.test(name)) {
-        error.errors.push({
-            fieldName: "name",
-            errorMessage: "Name must not contain a number"
-        })
-    }
+    if (/[0-9]+/.test(name)) throw new CreateNewError({ fieldName: "name", errorMessage: "Ім'я повинне містити виключно літери" }, "Validation error");
+    if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) throw new CreateNewError({ fieldName: "email", errorMessage: "Неправильна електронна адреса" }, "Validation error");
+    if (phoneNumber.length < 10 || phoneNumber.length > 15) throw new CreateNewError({ fieldName: "phoneNumber", errorMessage: "Неправильний номер телефону" }, "Validation error");
 
-    if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
-        error.errors.push({
-            fieldName: "email",
-            errorMessage: "Incorrect email adrres"
-        })
-    }
+    // passwor validation
+    if (password.length < 8) throw new CreateNewError({ fieldName: "password", errorMessage: "Пароль повинен містити мінімум 8 символів" }, "Validation error");
+    if (!/[a-z]/.test(password)) throw new CreateNewError({ fieldName: "password", errorMessage: "Пароль повинен містити хоча б одну малу літеру" }, "Validation error");
+    if (!/[A-Z]/.test(password)) throw new CreateNewError({ fieldName: "password", errorMessage: "Пароль повинен містити хоча б одну велику літеру" }, "Validation error");
+    if (!/[0-9]/.test(password)) throw new CreateNewError({ fieldName: "password", errorMessage: "Пароль повинен містити хоча б одну цифру" }, "Validation error");
+    if (!/[@$!%^&*_?]/.test(password)) throw new CreateNewError({ fieldName: "password", errorMessage: "Пароль повинен містити хоча б один спеціальний символ" }, "Validation error");
 
-    if (phoneNumber.length < 10 || phoneNumber.length > 15) {
-        error.errors.push({
-            fieldName: "phoneNumber",
-            errorMessage: "Incorrect phone number, it must have between 10 and 15 symbols"
-        })
-    } 
-
-    if (password.length < 8) {
-        error.errors.push({
-            fieldName: "password",
-            errorMessage: "password must contain 8 or more symbols"
-        })
-    } else if (!/[a-z]/.test(password)) {
-        error.errors.push({
-            fieldName: "password",
-            errorMessage: 'password must have at list one lowercase letter'
-        })
-    } else if (!/[A-Z]/.test(password)) {
-        error.errors.push({
-            fieldName: "password",
-            errorMessage: 'password must have at list one uppercase letter'
-        })
-    } else if (!/[0-9]/.test(password)) {
-        error.errors.push({
-            fieldName: "password",
-            errorMessage: 'password must have at list one digit'
-        })
-    } else if (!/[@$!%^&*_?]/.test(password)) {
-        error.errors.push({
-            fieldName: "password",
-            errorMessage: 'password must have at list one special symbol'
-        })
-
-    }
-
-    if (error.errors.length > 0) {
-        throw error
-    }
-    return true
-}
-
-
-
+    return true;
+};
